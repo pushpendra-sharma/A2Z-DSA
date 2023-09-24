7bba8e6302736e57f14cadfccf740d531ce71056
Pushpendra Sharma <pushpendrasharma400@gmail.com>
2023-09-24 19:35:16 +0530

    feat: solve pattern 2

--- basics/patterns/patterns.js
+++ basics/patterns/patterns.js
@@ -3,8 +3,7 @@
  * @param {number} n - The number of rows and columns in the pattern.
  * @returns {string} - The generated pattern as a string.
  */
-
-export function pattern1(n) {
+export function pattern1(n = 5) {
   let pattern = ``;
 
   for (let i = 0; i < n; i++) {
@@ -12,6 +11,24 @@ export function pattern1(n) {
       pattern += `* `;
     }
     pattern += `\n`;
+  }
+
+  return pattern;
+}
+
+/**
+ *
+ * @param {number} n - The number of rows and columns in the pattern.
+ * @returns {string} - The generated pattern as a string.
+ */
+export function pattern2(n = 5) {
+  let pattern = '';
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= i; j++) {
+      pattern += '* ';
+    }
+    pattern += '\n';
   }
 
   return pattern;
